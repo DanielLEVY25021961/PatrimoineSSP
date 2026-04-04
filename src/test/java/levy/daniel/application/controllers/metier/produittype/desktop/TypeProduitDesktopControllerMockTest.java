d4622e8a026bfbd533fa2280253a2c8ea241b0c2
Daniel Levy <dany.levy@free.fr>
2026-04-04 18:33:03 +0200

    Ajout des tests sur les controllers

---
+++ src/test/java/levy/daniel/application/controllers/metier/produittype/desktop/TypeProduitDesktopControllerMockTest.java
@@ -0,0 +1,61 @@
+package levy.daniel.application.controllers.metier.produittype.desktop;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * <style>p, ul, li, h1 {line-height : 1em;}</style>
+ * <style>h1 {text-decoration: underline;}</style>
+ * 
+ * 
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 4 avril 2026
+ */
+public class TypeProduitDesktopControllerMockTest {
+
+	// ************************ATTRIBUTS************************************/
+
+	/**
+	 * <style>p, ul, li {line-height : 1em;}</style>
+	 * <div>
+	 * <p>LOG : Logger : </p>
+	 * <p>Logger pour Log4j (utilisant org.apache.logging.log4j).</p>
+	 * <p>dépendances : </p>
+	 * <ul>
+	 * <li><code>org.apache.logging.log4j.Logger</code></li>
+	 * <li><code>org.apache.logging.log4j.LogManager</code></li>
+	 * </ul>
+	 * </div>
+	 */
+	private static final Logger LOG = LogManager
+			.getLogger(TypeProduitDesktopControllerMockTest.class);
+
+	// *************************METHODES************************************/
+
+
+
+	/**
+	* <div>
+	* <p>CONSTRUCTEUR D'ARITE NULLE.</p>
+	* </div>
+	*/
+	public TypeProduitDesktopControllerMockTest() {
+		super();
+	} // Fin du CONSTRUCTEUR D'ARITE NULLE.________________________________
+
+
+
+	@Test
+	final void test() {
+
+		fail("Not yet implemented"); // TODO
+
+	}
+
+}
---
+++ src/test/java/levy/daniel/application/controllers/metier/produittype/web/TypeProduitWebControllerMockTest.java
@@ -0,0 +1,63 @@
+package levy.daniel.application.controllers.metier.produittype.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * <style>p, ul, li, h1 {line-height : 1em;}</style>
+ * <style>h1 {text-decoration: underline;}</style>
+ * 
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 4 avril 2026
+ */
+public class TypeProduitWebControllerMockTest {
+
+	// ************************ATTRIBUTS************************************/
+
+	/**
+	 * <style>p, ul, li {line-height : 1em;}</style>
+	 * <div>
+	 * <p>LOG : Logger : </p>
+	 * <p>Logger pour Log4j (utilisant org.apache.logging.log4j).</p>
+	 * <p>dépendances : </p>
+	 * <ul>
+	 * <li><code>org.apache.logging.log4j.Logger</code></li>
+	 * <li><code>org.apache.logging.log4j.LogManager</code></li>
+	 * </ul>
+	 * </div>
+	 */
+	private static final Logger LOG = LogManager
+			.getLogger(TypeProduitWebControllerMockTest.class);
+
+	// *************************METHODES************************************/
+
+
+
+	/**
+	* <div>
+	* <p>CONSTRUCTEUR D'ARITE NULLE.</p>
+	* </div>
+	*/
+	public TypeProduitWebControllerMockTest() {
+
+		super();
+
+	} // Fin du CONSTRUCTEUR D'ARITE NULLE.________________________________
+
+
+
+	@Test
+	final void test() {
+
+		fail("Not yet implemented"); // TODO
+
+	}
+
+}
