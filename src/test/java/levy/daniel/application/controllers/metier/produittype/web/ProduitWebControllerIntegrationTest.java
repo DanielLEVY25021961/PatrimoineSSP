61be6f8d84f29499ecfdf9390078d28ac2a49ded
Daniel Levy <dany.levy@free.fr>
2026-04-09 23:07:22 +0200

    Après tests pour Desktop

---
+++ src/test/java/levy/daniel/application/controllers/metier/produittype/web/ProduitWebControllerIntegrationTest.java
@@ -0,0 +1,104 @@
+package levy.daniel.application.controllers.metier.produittype.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * <style>p, ul, li, h1 {line-height : 1em;}</style>
+ * <style>h1 {text-decoration: underline;}</style>
+ * 
+ * <div>
+ * <p style="font-weight:bold;">CLASSE ProduitWebControllerIntegrationTest.java :</p>
+ * 
+ * <p>
+ * Cette classe modélise : 
+ * <span style="font-weight:bold;">xxx</span> 
+ * yyy.
+ * </p>
+ * 
+ * <p>
+ * On y trouve :
+ * <ul>
+ * <li>exceptions.</li>
+ * <li>xxxx.</li>
+ * <li>yyy.</li>
+ * </ul>
+ * </p>
+ * 
+ * <p>
+ * xxx
+ * </p>
+ * <p>
+ * zzzz
+ * </p>
+ * <p>
+ * rrrr.
+ * </p>
+ * 
+ * </div>
+ *
+ * <ul>
+ * <li>
+ * <p>Exemple d'utilisation :</p>
+ * <p><code></code></p>
+ * </li>
+ *  
+ * <li>
+ * <p>Mots-clé :</p>
+ * <p></p>
+ * <p></p>
+ * <p></p>
+ * </li>
+ * </ul>
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 9 avr. 2026
+ */
+public class ProduitWebControllerIntegrationTest {
+
+	// ************************ATTRIBUTS************************************/
+
+	/**
+	 * <style>p, ul, li {line-height : 1em;}</style>
+	 * <div>
+	 * <p>LOG : Logger : </p>
+	 * <p>Logger pour Log4j (utilisant org.apache.logging.log4j).</p>
+	 * <p>dépendances : </p>
+	 * <ul>
+	 * <li><code>org.apache.logging.log4j.Logger</code></li>
+	 * <li><code>org.apache.logging.log4j.LogManager</code></li>
+	 * </ul>
+	 * </div>
+	 */
+	private static final Logger LOG = LogManager
+			.getLogger(ProduitWebControllerIntegrationTest.class);
+
+	// *************************METHODES************************************/
+
+
+
+	/**
+	* CONSTRUCTEUR D'ARITE NULLE.
+	*/
+	public ProduitWebControllerIntegrationTest() {
+
+		super();
+
+	} // Fin du CONSTRUCTEUR D'ARITE NULLE.________________________________
+
+
+
+	@Test
+	final void test() {
+
+		fail("Not yet implemented"); // TODO
+
+	}
+
+}
